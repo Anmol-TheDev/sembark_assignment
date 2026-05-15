9734c6c9d3c795e7a69ca721b691a99ea18f5f60
anmol-thedev <anmolsingh79102@gmail.com>
2026-05-15 14:13:52 +0530

    feat: api setup for category and products

---
+++ features/products/product.action.ts
@@ -0,0 +1,28 @@
+import axiosClient from '@/lib/axios';
+
+export interface Category {
+  id: number;
+  name: string;
+  image: string;
+  slug: string;
+}
+
+export interface Product {
+  id: number;
+  title: string;
+  slug: string;
+  price: number;
+  description: string;
+  category: Category;
+  images: string[];
+}
+
+export const getProducts = async (): Promise<Product[]> => {
+  const response = await axiosClient.get<Product[]>('/products');
+  return response.data;
+};
+
+export const getProductBySlug = async (slug: string): Promise<Product> => {
+  const response = await axiosClient.get<Product>(`/products/slug/${slug}`);
+  return response.data;
+};
